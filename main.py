601054c85bd55c886bc50a06d8ff769d476ebe3d
bayubramasta <bayubramasta125@gmail.com>
2026-06-24 13:12:42 +0700

    Adding Zoom feature & fix font chooser

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import tkinter.scrolledtext as st
 from tkinter import *
 from tkinter import filedialog
 from tkinter import font
@@ -20,37 +21,70 @@ def save_popup():
     x = (screen_width // 2) - (250 // 2)
     y = (screen_height // 2) - (100 // 2)
     popup.geometry(f"250x100+{x}+{y}")
+    popup.resizable(False, False)
 
     tk.Label(popup, text="Saving...").pack(expand=True)
     popup.after(1500, popup.destroy)
-def font_popup():
-    popup = tk.Toplevel(root)
-    popup.title("Fonts")
-    # make window appear on center of screen
 
-    # Get screen size
-    screen_width = root.winfo_screenwidth()
-    screen_height = root.winfo_screenheight()
+# change font here
+def font_popup(event=None):
+    if event is None:
+        popup = tk.Toplevel(root)
+        popup.title("Fonts")
+        # make window appear on center of screen
 
-    # Count center of screen
-    x = (screen_width // 2) - (250 // 2)
-    y = (screen_height // 2) - (100 // 2)
-    popup.geometry(f"250x100+{x}+{y}")
+        # Get screen size
+        screen_width = root.winfo_screenwidth()
+        screen_height = root.winfo_screenheight()
 
-    # Font type
-    tk.Label(popup, text="Fonts").grid(row=0, column=0,padx=5, pady=5)
-    fonts = sorted(font.families())
-    fontchoosen = ttk.Combobox(popup, width=27, values=fonts)
-    fontchoosen.grid(column=1, row=0)
+        # Count center of screen
+        x = (screen_width // 2) - (250 // 2)
+        y = (screen_height // 2) - (100 // 2)
+        popup.geometry(f"250x100+{x}+{y}")
+        popup.resizable(False, False)
 
-    # Font size
-    tk.Label(popup, text="Size").grid(row=1, column=0, padx=5, pady=5)
-    fontsize = tk.Text(popup, height=1, width=23 )
-    fontsize.grid(column=1, row=1)
+        # Current text editor font
+        f = font.Font(font=textEditor.cget("font"))
 
-    # Button
-    (tk.Button(popup, text="Change", command=lambda :textEditor.configure(font = tk.font.Font(family=fontchoosen.get(), size=fontsize.get('1.0',tk.END), weight="normal")))
-                 .grid(row=2, column=0, columnspan=2, pady=5))
+        # Font type
+        tk.Label(popup, text="Fonts").grid(row=0, column=0,padx=5, pady=5)
+        fonts = sorted(font.families())
+        fontchoosen = ttk.Combobox(popup, width=27, values=fonts,font=('Tahoma',8,'normal'))
+        fontchoosen.current(fonts.index(f.actual("family")))
+        fontchoosen.grid(column=1, row=0)
+
+        # Font size
+        tk.Label(popup, text="Size").grid(row=1, column=0, padx=5, pady=5)
+        fontsize = tk.Text(popup, height=1, width=30, font=('Tahoma',8,'normal'))
+        fontsize.insert('0.0', str(f.actual("size")))
+        fontsize.grid(column=1, row=1)
+
+        # Button
+        (tk.Button(popup, text="Change", command=lambda :[textEditor.configure(
+            font = tk.font.Font(family=fontchoosen.get(), size=int(fontsize.get('1.0',tk.END)), weight="normal")),popup.destroy()])
+                     .grid(row=2, column=0, columnspan=2, pady=5))
+    else:
+        if event.keysym == "equal":
+            f = font.Font(font=textEditor.cget("font"))
+            fontsize = int(f.actual("size"))+1
+            textEditor.configure(font=tk.font.Font(family=f.actual("family"), size=int(fontsize)))
+        else:
+            f = font.Font(font=textEditor.cget("font"))
+            fontsize = int(f.actual("size")) - 1
+            textEditor.configure(font=tk.font.Font(family=f.actual("family"), size=int(fontsize)))
+# Status bar
+def update_status(event=None):
+    current_font = font.Font(font=textEditor.cget("font"))
+    line, col = textEditor.index("insert").split(".")
+    status.config(
+        text=f"Ln {line}, Col {col} | {current_font.actual("family")} {current_font.actual("size")} "
+    )
+def poll_cursor():
+    update_status()
+    root.after(100, poll_cursor)
+
+
+
 
 #save and open
 def closeWindow():
@@ -74,21 +108,25 @@ def closeWindow():
                 save()
     else:
         root.destroy()
-def save():
+def save(event=None):
     if root.title().split()[2] == 'New':
         filename = filedialog.asksaveasfile(initialfile='Untitled.txt',
                               defaultextension=".txt", filetypes=[("All Files", "*.*"),
                             ("Text Documents", "*.txt")])
-        save_popup()
-        with open(filename.name, "a") as f:
-            f.write(textEditor.get('0.0', tk.END))
-        root.title("Text Editor "+str(filename.name))
+        if filename:
+            save_popup()
+            with open(filename.name, "a") as f:
+                f.write(textEditor.get('0.0', tk.END))
+            root.title("Text Editor "+str(filename.name))
+        else:
+            # do nothing
+            pass
     else:
         save_popup()
         with open(root.title().split()[2], "w") as f:
             f.write(textEditor.get('0.0', tk.END))
         root.title("Text Editor "+str(root.title().split()[2]))
-def load():
+def load(event=None):
     if len(textEditor.get('0.0', tk.END)) > 1:
         title = root.title().split()[2]
         if title != 'New':
@@ -201,11 +239,31 @@ root.config(menu = menubar)
 
 #body session
 body = tk.Frame(root)
-textEditor = tk.Text(body)
+textEditor = st.ScrolledText(body)
 textEditor.configure(font = tk.font.Font(family="Arial", size=14, weight="normal"))
 textEditor.pack(fill="both",expand=True)
 body.pack(fill="both",expand=True)
+body.pack_propagate(False)
+
+
+# Footer session
+footer = tk.Frame(root)
+tk.Label(footer, text = "Text Editor 1.0").pack(side = tk.LEFT)
+status = tk.Label(footer, text = textEditor.index("insert"))
+status.pack(side = tk.RIGHT)
+footer.pack(fill="x", side=tk.BOTTOM)
 
 #window event
+
+# save project shortcut
+root.bind("<Control-s>",save)
+# zoom in & out text editor shortcut
+root.bind("<Control-=>",font_popup)
+root.bind("<Control-_>",font_popup)
+# open file
+root.bind("<Control-o>",load)
+
 root.protocol("WM_DELETE_WINDOW",closeWindow)
+# status bar function
+poll_cursor()
 root.mainloop()
